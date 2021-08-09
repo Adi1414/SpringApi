8c1f86d0ef0a98eba5b10b9743455265192aa223
Aditi Agrawal <aditi.agrawal@thoughtworks.com>
2021-08-09 20:37:30 +0530

    Add. controllers for HATEOAS Order and User

--- src/main/java/com/example/spingApi/controller/OrderHateoasController.java
+++ src/main/java/com/example/spingApi/controller/OrderHateoasController.java
@@ -1,2 +1,32 @@
-package com.example.spingApi.controller;public class OrderHateoasController {
+package com.example.spingApi.controller;
+
+import com.example.spingApi.exception.UserNotFoundException;
+import com.example.spingApi.repository.UserRepository;
+import com.example.spingApi.restServices.Order;
+import com.example.spingApi.restServices.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/users/hateoas")
+public class OrderHateoasController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @GetMapping("/{id}/orders")
+    public List<Order> getAllOrders(@PathVariable("id") Long id) throws UserNotFoundException {
+        Optional<User> user = userRepository.findById(id);
+        if(!user.isPresent()){
+            throw new UserNotFoundException("User not found");
+        }
+        return user.get().getOrders();
+    }
+
 }
--- src/main/java/com/example/spingApi/controller/UserHateoasController.java
+++ src/main/java/com/example/spingApi/controller/UserHateoasController.java
@@ -1,2 +1,42 @@
-package com.example.spingApi.controller;public class UserHateoasController {
+package com.example.spingApi.controller;
+
+import com.example.spingApi.exception.UserNotFoundException;
+import com.example.spingApi.restServices.User;
+import com.example.spingApi.restServices.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
+import javax.validation.constraints.Min;
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("users/hateoas")
+@Validated
+public class UserHateoasController {
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping
+    public List<User> getAllUsers(){
+        return userService.getAllUsers();
+    }
+
+    @GetMapping("/{id}")
+    public Optional<User> getUserByID(@PathVariable("id") @Min(1) Long id){
+        try{
+            return userService.getUserById(id);
+        }
+        catch(UserNotFoundException ex){
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, ex.getMessage());
+        }
+    }
+
 }
